6a0e0a56c8aac7fa67a3edd74ab8f6e3afce2f63
chanchaw <chan_chaw@126.com>
2021-01-08 10:23:41 +0800

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import {TableModule} from 'primeng/table';
+import {ToastModule} from 'primeng/toast';
+import {CalendarModule} from 'primeng/calendar';
+import {SliderModule} from 'primeng/slider';
+import {MultiSelectModule} from 'primeng/multiselect';
+import {ContextMenuModule} from 'primeng/contextmenu';
+import {DialogModule} from 'primeng/dialog';
+import {ButtonModule} from 'primeng/button';
+import {DropdownModule} from 'primeng/dropdown';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,AppRoutingModule,
+    FormsModule,HttpClientModule,BrowserAnimationsModule,
+    TableModule,ToastModule,CalendarModule,SliderModule,MultiSelectModule,
+    MultiSelectModule,ContextMenuModule,DialogModule,ButtonModule,DropdownModule,
+    ProgressBarModule,InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
